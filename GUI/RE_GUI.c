1cd66b218393be3c887379edd55e77dc8ee85fc6
Duc <dangminhduc1101@gmail.com>
2022-09-09 16:23:13 -0400

    update .gitignore

--- GUI/RE_GUI.c
+++ GUI/RE_GUI.c
@@ -5,7 +5,7 @@ This program operates a kipr-wombat-based robot (equipped with analog photo, ir,
 
 Course:			211 - Perception & Action
 Instructors:	Ken Livingston, Joshua de Leeuw
-Authors: 		Nick Livingston (old KIPR Link version), Duc Dang (new KIPR Wombat version)
+Authors: 		Ryan Luke Johns (old KIPR Link version), Duc Dang (new KIPR Wombat version)
 Date:			August 2022
 */
 
@@ -30,14 +30,6 @@ Date:			August 2022
 #define RIGHT_MOTOR_PIN 0
 #define LEFT_MOTOR_PIN 1 // servos
 
-#define SEEK_LIGHT_TYPE 0
-#define SEEK_DARK_TYPE 1
-#define APPROACH_TYPE 2
-#define AVOID_TYPE 3
-#define ESCAPE_F_TYPE 4
-#define ESCAPE_B_TYPE 5
-#define CRUISE_S_TYPE 6
-#define CRUISE_A_TYPE 7 // GUI-related action types
 
 // *** Function Declarations *** //
 
@@ -86,6 +78,237 @@ int photo_threshold = 150;	   // the absolute difference between photo sensor re
 // timer
 int timer_duration = 500;	  // the time in milliseconds to wait between calling action commands, changed by each drive command called by actions
 unsigned long start_time = 0; // store the system time each time we start an action so we can see if our time has elapsed without a blocking delay
+
+//==============================================//
+//===============GUI RELATED CODE===============//
+//==============================================//
+
+// Define GUI-related action types
+#define SEEK_LIGHT_TYPE 0
+#define SEEK_DARK_TYPE 1
+#define APPROACH_TYPE 2
+#define AVOID_TYPE 3
+#define ESCAPE_F_TYPE 4
+#define ESCAPE_B_TYPE 5
+#define CRUISE_S_TYPE 6
+#define CRUISE_A_TYPE 7
+
+/*
+Here, we define new kind of variable type called "behavior" that contains properties for type (indexing definitions above), rank, and an active/inactive boolean.
+*/
+typedef struct behavior
+{
+	const char *title;
+	int type;
+	int rank;
+	bool is_active;
+} behavior;
+
+/*
+This struct defines the initial behavior, but also describes all possible behaviors.
+New ones could be added, or the order can be moved around.
+This behavior runs once at the beginning of the program until the gui is accessed.
+There is no need to change the rank value manually, just change the order and set
+the ones you want to be active to "true".  The element at the top is at the top of the hierarchy.
+*/
+struct behavior subsumption_hierarchy[] = {
+	{"ESCAPE FRONT", ESCAPE_F_TYPE, 0, true},
+	{"ESCAPE BACK", ESCAPE_B_TYPE, 0, true},
+	{"AVOID", AVOID_TYPE, 0, true},
+	{"SEEK LIGHT", SEEK_LIGHT_TYPE, 0, true},
+	{"CRUISE STRAIGHT", CRUISE_S_TYPE, 0, true},
+	{"SEEK DARK", SEEK_DARK_TYPE, 0, false},
+	{"APPROACH", APPROACH_TYPE, 0, false},
+	{"CRUISE ARC", CRUISE_A_TYPE, 0, false}};
+
+
+/*
+Declare and initialize variables
+*/
+int hierarchy_length;				   // set in main function based on number of elements in subsumption_hierarchy defined above
+int cursor_row = 0;					   // the row that the cursor is on in gui mode
+bool show_gui = false;				   // boolean toggled by pushing the white side button on the kipr link
+bool first_gui = true;				   // on first exposure to gui, we randomize the hierarchy so the initialized behavior can't be observed
+bool is_side_update = false;		   // sort on button press
+bool update_operating_console = false; // a boolean to tell us when to update the operating console, preventing us from constantly reprinting and clearing, which results in flicker
+
+/*
+This comparator function is used in the qsort function for sorting our behavior list.
+Active things always go before inactive things, and if both are active then the are ordered by rank.
+*/
+int compare_ranks(const void *a, const void *b)
+{
+	bool is_active_a = ((struct behavior *)a)->is_active;
+	bool is_active_b = ((struct behavior *)b)->is_active;
+	if (is_active_a != is_active_b)
+	{
+		return is_active_b;
+	}
+	int rank_a = ((struct behavior *)a)->rank;
+	int rank_b = ((struct behavior *)b)->rank;
+	return (rank_a - rank_b);
+}
+
+/* MANAGE GUI UPDATE */
+void update_gui()
+{
+	if (side_button_clicked())
+	{
+		show_gui = !show_gui;			 // toggle our gui by pressing the side button
+		is_side_update = show_gui;		 // boolean to do certain behaviors once at button press
+		update_operating_console = true; // boolean to update the home console once
+	}
+
+	if (show_gui)
+	{
+		if (first_gui)
+		{
+			randomize_hierarchy(); // this only ever happens once per program
+			first_gui = false;
+		}
+
+		bool cursor_update = false;
+		bool hierarchy_update = false;
+
+		set_extra_buttons_visible(1); // we turn off the extra buttons (buttons xyz) when we are not in showgui mode, so we need to activate them here
+
+		set_a_button_text(subsumption_hierarchy[cursor_row].is_active ? "Deactivate" : "Activate"); // set text to display activate or deactivate based on the behavior the cursor is on
+		set_b_button_text(subsumption_hierarchy[cursor_row].is_active ? "Move Up" : "");			// set text to display "move up" or nothing based on the behavior the cursor is on
+		set_y_button_text(subsumption_hierarchy[cursor_row].is_active ? "Move Down" : "");			// set text to display "move down" or nothing based on the behavior the cursor is on
+
+		set_c_button_text("\u25B2"); // up triangle unicode
+		set_z_button_text("\u25BC"); // unicode down triangle
+
+		set_x_button_text("Reset"); // reset button for deactivating all
+
+		if (c_button_clicked())
+		{								   // move the cursor up
+			cursor_row = (cursor_row - 1); // up cursor
+			if (cursor_row < 0)
+				cursor_row += hierarchy_length; // if we go past zero, loop back to the end of the list
+			cursor_row = cursor_row % hierarchy_length;
+			cursor_update = true; // we've updated
+		}
+
+		else if (z_button_clicked())
+		{													  // move cursor down
+			cursor_row = (cursor_row + 1) % hierarchy_length; // move cursor down and use modulus function to loop back to zero if we go down too far
+			cursor_update = true;
+		}
+
+		else if (a_button_clicked())
+		{																								// activate or deactivate button
+			subsumption_hierarchy[cursor_row].is_active = !subsumption_hierarchy[cursor_row].is_active; // toggle our active state
+			hierarchy_update = true;
+		}
+
+		else if (b_button_clicked())
+		{
+			subsumption_hierarchy[cursor_row].rank -= 2; // move up
+			hierarchy_update = true;
+		}
+
+		else if (y_button_clicked())
+		{
+			subsumption_hierarchy[cursor_row].rank += 2; // move down
+			hierarchy_update = true;
+		}
+
+		else if (x_button_clicked())
+		{ // reset all button
+			size_t i;
+			for (i = 0; i < hierarchy_length; i++)
+			{
+				subsumption_hierarchy[i].is_active = false;
+			}
+			hierarchy_update = true;
+		}
+
+		if (cursor_update || is_side_update || hierarchy_update)
+		{ // if we pressed anything at all
+
+			qsort(subsumption_hierarchy, hierarchy_length, sizeof(behavior), compare_ranks); // sort our hierarchy based on rank value
+
+			size_t i;
+			for (i = 0; i < hierarchy_length; i++)
+			{
+				if (subsumption_hierarchy[i].is_active)
+					subsumption_hierarchy[i].rank = i; // now reset the index of each sorted active behavior to be sequential with a step size of one
+				else
+					subsumption_hierarchy[i].rank = hierarchy_length + 1; // give inactive behaviors a constant "poor" rank which is helpful to ensure new ones always jump above.
+			}
+
+			console_clear();													  // clear the console
+			print_subsumption_hierarchy(subsumption_hierarchy, hierarchy_length); // print the hierarchy and interface
+			is_side_update = false;												  // turn off the is_side_update boolean so we don't get screen flicker until we update the cursor or hierarchy next
+		}
+	}
+	else
+	{
+		set_a_button_text(""); // if we are not in show_gui mode, we must be operating, set our buttons to show nothing and hide the extra buttons
+		set_b_button_text("");
+		set_c_button_text("");
+		set_extra_buttons_visible(0);
+	}
+}
+/* RANDOMIZE HIERARCHY AND DEACTIVATE ALL */
+void randomize_hierarchy()
+{
+	size_t i;
+	for (i = 0; i < hierarchy_length; i++)
+	{
+		subsumption_hierarchy[i].is_active = false;
+		subsumption_hierarchy[i].rank = rand();
+	}
+	qsort(subsumption_hierarchy, hierarchy_length, sizeof(behavior), compare_ranks); // sort our hierarchy based on rank value
+}
+
+/* MANAGE SCREEN PRINTING OF GUI */
+void print_subsumption_hierarchy(struct behavior *array, size_t len)
+{
+	size_t i;
+	for (i = 0; i < len; i++)
+	{
+		display_printf(1, i, "%s          ", array[i].title);
+		if (array[i].is_active)
+		{
+			display_printf(17, i, "Active  ");
+		}
+		else
+		{
+			display_printf(17, i, "Inactive ");
+		}
+		if (i == cursor_row)
+		{
+			display_printf(0, i, ">");
+			display_printf(25, i, "<");
+		}
+		else
+		{
+			display_printf(0, i, " ");
+			display_printf(25, i, " ");
+		}
+		// display_printf(35, i, "%d", array[i].rank); //debug for showing rank
+	}
+}
+
+/* MANAGE SCREEN PRINTING WHEN OPERATING */
+void print_set_hierarchy()
+{
+	if (update_operating_console && !first_gui)
+	{
+		console_clear();
+		size_t i;
+		for (i = 0; i < hierarchy_length; i++)
+		{
+			if (subsumption_hierarchy[i].is_active)
+				printf(" %s\n", subsumption_hierarchy[i].title);
+		}
+		update_operating_console = false; // this only happens once per button press if we are not showing gui
+	}
+}
+
+//===============END GUI-RELATED CODE===============//
 
 // *** Function Definitions *** //
 
@@ -355,218 +578,3 @@ float map(float value, float start_range_low, float start_range_high, float targ
 	// remap a value from a source range to a new range
 }
 
-//==============================================//
-//===============GUI RELATED CODE===============//
-//==============================================//
-
-/*
-Here, we define new kind of variable type called "behavior" that contains properties for type (indexing definitions above), rank, and an active/inactive boolean.
-*/
-
-typedef struct behavior
-{
-	const char *title;
-	int type;
-	int rank;
-	bool is_active;
-} behavior;
-
-/*
-This struct defines the initial behavior, but also describes all possible behaviors.
-New ones could be added, or the order can be moved around.
-This behavior runs once at the beginning of the program until the gui is accessed.
-There is no need to change the rank value manually, just change the order and set
-the ones you want to be active to "true".  The element at the top is at the top of the hierarchy.
-*/
-
-struct behavior subsumption_hierarchy[] = {
-	{"ESCAPE FRONT", ESCAPE_F_TYPE, 0, true},
-	{"ESCAPE BACK", ESCAPE_B_TYPE, 0, true},
-	{"AVOID", AVOID_TYPE, 0, true},
-	{"SEEK LIGHT", SEEK_LIGHT_TYPE, 0, true},
-	{"CRUISE STRAIGHT", CRUISE_S_TYPE, 0, true},
-	{"SEEK DARK", SEEK_DARK_TYPE, 0, false},
-	{"APPROACH", APPROACH_TYPE, 0, false},
-	{"CRUISE ARC", CRUISE_A_TYPE, 0, false}};
-
-int hierarchy_length;				   // set in main function based on number of elements in subsumption_hierarchy defined above
-int cursor_row = 0;					   // the row that the cursor is on in gui mode
-bool show_gui = false;				   // boolean toggled by pushing the white side button on the kipr link
-bool first_gui = true;				   // on first exposure to gui, we randomize the hierarchy so the initialized behavior can't be observed
-bool is_side_update = false;		   // sort on button press
-bool update_operating_console = false; // a boolean to tell us when to update the operating console, preventing us from constantly reprinting and clearing, which results in flicker
-
-/*
-This comparator function is used in the qsort function for sorting our behavior list.
-Active things always go before inactive things, and if both are active then the are ordered by rank.
-*/
-
-int compare_ranks(const void *a, const void *b)
-{
-	bool is_active_a = ((struct behavior *)a)->is_active;
-	bool is_active_b = ((struct behavior *)b)->is_active;
-	if (is_active_a != is_active_b)
-	{
-		return is_active_b;
-	}
-	int rank_a = ((struct behavior *)a)->rank;
-	int rank_b = ((struct behavior *)b)->rank;
-	return (rank_a - rank_b);
-}
-
-void update_gui()
-{
-	if (side_button_clicked())
-	{
-		show_gui = !show_gui;			 // toggle our gui by pressing the side button
-		is_side_update = show_gui;		 // boolean to do certain behaviors once at button press
-		update_operating_console = true; // boolean to update the home console once
-	}
-
-	if (show_gui)
-	{
-		if (first_gui)
-		{
-			randomize_hierarchy(); // this only ever happens once per program
-			first_gui = false;
-		}
-
-		bool cursor_update = false;
-		bool hierarchy_update = false;
-
-		set_extra_buttons_visible(1); // we turn off the extra buttons (buttons xyz) when we are not in showgui mode, so we need to activate them here
-
-		set_a_button_text(subsumption_hierarchy[cursor_row].is_active ? "Deactivate" : "Activate"); // set text to display activate or deactivate based on the behavior the cursor is on
-		set_b_button_text(subsumption_hierarchy[cursor_row].is_active ? "Move Up" : "");			// set text to display "move up" or nothing based on the behavior the cursor is on
-		set_y_button_text(subsumption_hierarchy[cursor_row].is_active ? "Move Down" : "");			// set text to display "move down" or nothing based on the behavior the cursor is on
-
-		set_c_button_text("\u25B2"); // up triangle unicode
-		set_z_button_text("\u25BC"); // unicode down triangle
-
-		set_x_button_text("Reset"); // reset button for deactivating all
-
-		if (c_button_clicked())
-		{								   // move the cursor up
-			cursor_row = (cursor_row - 1); // up cursor
-			if (cursor_row < 0)
-				cursor_row += hierarchy_length; // if we go past zero, loop back to the end of the list
-			cursor_row = cursor_row % hierarchy_length;
-			cursor_update = true; // we've updated
-		}
-
-		else if (z_button_clicked())
-		{													  // move cursor down
-			cursor_row = (cursor_row + 1) % hierarchy_length; // move cursor down and use modulus function to loop back to zero if we go down too far
-			cursor_update = true;
-		}
-
-		else if (a_button_clicked())
-		{																								// activate or deactivate button
-			subsumption_hierarchy[cursor_row].is_active = !subsumption_hierarchy[cursor_row].is_active; // toggle our active state
-			hierarchy_update = true;
-		}
-
-		else if (b_button_clicked())
-		{
-			subsumption_hierarchy[cursor_row].rank -= 2; // move up
-			hierarchy_update = true;
-		}
-
-		else if (y_button_clicked())
-		{
-			subsumption_hierarchy[cursor_row].rank += 2; // move down
-			hierarchy_update = true;
-		}
-
-		else if (x_button_clicked())
-		{ // reset all button
-			size_t i;
-			for (i = 0; i < hierarchy_length; i++)
-			{
-				subsumption_hierarchy[i].is_active = false;
-			}
-			hierarchy_update = true;
-		}
-
-		if (cursor_update || is_side_update || hierarchy_update)
-		{ // if we pressed anything at all
-
-			qsort(subsumption_hierarchy, hierarchy_length, sizeof(behavior), compare_ranks); // sort our hierarchy based on rank value
-
-			size_t i;
-			for (i = 0; i < hierarchy_length; i++)
-			{
-				if (subsumption_hierarchy[i].is_active)
-					subsumption_hierarchy[i].rank = i; // now reset the index of each sorted active behavior to be sequential with a step size of one
-				else
-					subsumption_hierarchy[i].rank = hierarchy_length + 1; // give inactive behaviors a constant "poor" rank which is helpful to ensure new ones always jump above.
-			}
-
-			console_clear();													  // clear the console
-			print_subsumption_hierarchy(subsumption_hierarchy, hierarchy_length); // print the hierarchy and interface
-			is_side_update = false;												  // turn off the is_side_update boolean so we don't get screen flicker until we update the cursor or hierarchy next
-		}
-	}
-	else
-	{
-		set_a_button_text(""); // if we are not in show_gui mode, we must be operating, set our buttons to show nothing and hide the extra buttons
-		set_b_button_text("");
-		set_c_button_text("");
-		set_extra_buttons_visible(0);
-	}
-}
-//-----------------RANDOMIZE HIERARCHY AND DEACTIVATE ALL-------------
-void randomize_hierarchy()
-{
-	size_t i;
-	for (i = 0; i < hierarchy_length; i++)
-	{
-		subsumption_hierarchy[i].is_active = false;
-		subsumption_hierarchy[i].rank = rand();
-	}
-	qsort(subsumption_hierarchy, hierarchy_length, sizeof(behavior), compare_ranks); // sort our hierarchy based on rank value
-}
-//-------------------------MANAGE SCREEN PRINTING OF GUI--------------------
-void print_subsumption_hierarchy(struct behavior *array, size_t len)
-{
-	size_t i;
-	for (i = 0; i < len; i++)
-	{
-		display_printf(1, i, "%s          ", array[i].title);
-		if (array[i].is_active)
-		{
-			display_printf(17, i, "Active  ");
-		}
-		else
-		{
-			display_printf(17, i, "Inactive ");
-		}
-		if (i == cursor_row)
-		{
-			display_printf(0, i, ">");
-			display_printf(25, i, "<");
-		}
-		else
-		{
-			display_printf(0, i, " ");
-			display_printf(25, i, " ");
-		}
-		// display_printf(35, i, "%d", array[i].rank); //debug for showing rank
-	}
-}
-//--------------------MANAGE SCREEN PRINTING WHEN OPERATING---------------------
-void print_set_hierarchy()
-{
-	if (update_operating_console && !first_gui)
-	{
-		console_clear();
-		size_t i;
-		for (i = 0; i < hierarchy_length; i++)
-		{
-			if (subsumption_hierarchy[i].is_active)
-				printf(" %s\n", subsumption_hierarchy[i].title);
-		}
-		update_operating_console = false; // this only happens once per button press if we are not showing gui
-	}
-}
-//============================END GUI RELATED CODE========================================
